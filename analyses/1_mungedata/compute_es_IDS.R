028d1a83cd7a127699b8818eb22dea5ab2d15b17
Molly Lewis <mollyllewis@gmail.com>
2020-06-19 12:57:58 -0400

    update compute es script2

--- analyses/1_mungedata/compute_es_IDS.R
+++ analyses/1_mungedata/compute_es_IDS.R
@@ -1,3 +1,5 @@
+# calculate ES for IDS MA
+
 library(tidyverse)
 library(assertthat)
 
@@ -5,7 +7,6 @@ complete <- function(...) {
   args = list(...)
   !any(unlist(map(args, ~(is.null(.x) || is.na(.x)))))
 }
-
 
 compute_es <- function(ma_df) {
 
@@ -19,25 +20,24 @@ compute_es <- function(ma_df) {
   d = ma_df$d
   d_var = ma_df$d_var
 
-    assert_that(participant_design %in% c("between", "within_two"))
+  assert_that(participant_design %in% c("between", "within_two"))
 
-    d_calc <- NA
-    d_var_calc <- NA
-    es_method <- "missing"
+  d_calc <- NA
+  d_var_calc <- NA
+  es_method <- "missing"
 
-    #effect size calculation
-   if (complete(x_1, x_2, n_1, n_2, SD_1, SD_2)) {
-      pooled_SD <- sqrt((((n_1 - 1) * SD_1 ^ 2) + ((n_2 - 1) * SD_2 ^ 2)) / (n_1 + n_2 - 2)) # "classic" Cohen's d with marginal SD
-      d_calc <- (x_1 - x_2) / pooled_SD
-      d_var_calc <- ((n_1 + n_2) / (n_1 * n_2)) + (d_calc ^ 2 / (2 * (n_1 + n_2)))
-      es_method  <- "classic_cohen_d"
+  #effect size calculation
+ if (complete(x_1, x_2, n_1, n_2, SD_1, SD_2)) {
+    pooled_SD <- sqrt((((n_1 - 1) * SD_1 ^ 2) + ((n_2 - 1) * SD_2 ^ 2)) / (n_1 + n_2 - 2)) # "classic" Cohen's d with marginal SD
+    d_calc <- (x_1 - x_2) / pooled_SD
+    d_var_calc <- ((n_1 + n_2) / (n_1 * n_2)) + (d_calc ^ 2 / (2 * (n_1 + n_2)))
+    es_method  <- "classic_cohen_d"
 
-    } else if (complete(d, d_var)) { # if raw data are not available do we want to use d already reported (using who knows what method)? I'd vote for no.
-      d_calc <- d
-      d_var_calc <- d_var
-      es_method  <- "reported_d"
-    }
-
+  } else if (complete(d, d_var)) { # if raw data are not available do we want to use d already reported (using who knows what method)? I'd vote for no.
+    d_calc <- d
+    d_var_calc <- d_var
+    es_method  <- "reported_d"
+  }
 
   data_frame("d_calc" = d_calc,
              "d_var_calc" = d_var_calc,
