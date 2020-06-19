219c60320611261cb9be5d3a176adcaf2360e9c1
Molly Lewis <mollyllewis@gmail.com>
2020-06-19 12:55:14 -0400

    update compute es script

--- analyses/1_mungedata/compute_es_IDS.R
+++ analyses/1_mungedata/compute_es_IDS.R
@@ -1,6 +1,4 @@
-
-library(dplyr)
-library(purrr)
+library(tidyverse)
 library(assertthat)
 
 complete <- function(...) {
@@ -21,57 +19,25 @@ compute_es <- function(ma_df) {
   d = ma_df$d
   d_var = ma_df$d_var
 
-
-
     assert_that(participant_design %in% c("between", "within_two"))
 
-    #we introduce variables calles d_calc and d_var_calc to distiguish them from the fields d and d_var, which are fields where effect sizes were already available from the source of the data
     d_calc <- NA
     d_var_calc <- NA
     es_method <- "missing"
 
-    if (participant_design == "between") {
+    #effect size calculation
+   if (complete(x_1, x_2, n_1, n_2, SD_1, SD_2)) {
+      pooled_SD <- sqrt((((n_1 - 1) * SD_1 ^ 2) + ((n_2 - 1) * SD_2 ^ 2)) / (n_1 + n_2 - 2)) # "classic" Cohen's d with marginal SD
+      d_calc <- (x_1 - x_2) / pooled_SD
+      d_var_calc <- ((n_1 + n_2) / (n_1 * n_2)) + (d_calc ^ 2 / (2 * (n_1 + n_2)))
+      es_method  <- "classic_cohen_d"
 
-        #effect size calculation
-       if (complete(x_1, x_2, SD_1, SD_2)) {
-          pooled_SD <- sqrt(((n_1 - 1) * SD_1 ^ 2 + (n_2 - 1) * SD_2 ^ 2) / (n_1 + n_2 - 2)) # Lipsey & Wilson, 3.14
-          d_calc <- (x_1 - x_2) / pooled_SD # Lipsey & Wilson (2001)
-          es_method  <- "group_means_between"
-
-        } else if (complete(d)) {
-          d_calc <- d
-          es_method  <- "d_between"
-        }
-
-        #effect size var calculation
-        if (complete(n_1, n_2, d_calc)) {
-          d_var_calc <- ((n_1 + n_2) / (n_1 * n_2)) + (d_calc ^ 2 / (2 * (n_1 + n_2)))
-        } else if (complete(d_var)) {
-          d_var_calc <- d_var
-        }
-
-    } else if (participant_design == "within_two") {
-
-        #effect size calculation
-       if (complete(x_1, x_2, SD_1, SD_2)) {
-            pooled_SD <- sqrt((SD_1 ^ 2 + SD_2 ^ 2) / 2) # Lipsey & Wilson (2001)
-            d_calc <- (x_1 - x_2) / pooled_SD # Lipsey & Wilson (2001)
-            es_method  <- "group_means_two"
-
-       } else if (complete(d)) {
-         d_calc <- d
-         es_method  <- "d_two"
-       }
-
-      #effect size var calculation
-      if (complete(n_1,  d_calc)) {
-        d_var_calc <-   (2/n_1) + (d_calc ^ 2 / (4 * n_1))
-
-      } else if (complete(d_var)){
-        d_var_calc <- d_var
-      }
-
+    } else if (complete(d, d_var)) { # if raw data are not available do we want to use d already reported (using who knows what method)? I'd vote for no.
+      d_calc <- d
+      d_var_calc <- d_var
+      es_method  <- "reported_d"
     }
+
 
   data_frame("d_calc" = d_calc,
              "d_var_calc" = d_var_calc,
