7a6c513aa7afd91196820717f7e54a7a0f8fa258
mzettersten <mzettersten@gmail.com>
2022-01-07 16:46:38 -0500

    fix error in pooled_SD computation (issue #20)
    - also some minor rewordings on the es_method classification

--- analyses/1_mungedata/compute_es_IDS.R
+++ analyses/1_mungedata/compute_es_IDS.R
@@ -36,10 +36,10 @@ compute_es <- function(ma_df) {
     es_method  <- "classic_cohen_d_between"
     
   } else if (participant_design == "within_two" & complete(x_1, x_2, n_1, SD_1, SD_2)) {
-    pooled_SD <- sqrt((SD_1 ^ 2) + (SD_2 ^ 2) / 2) 
+    pooled_SD <- sqrt((SD_1 ^ 2 + SD_2 ^ 2) / 2) 
     d_calc <- (x_1 - x_2) / pooled_SD
     d_var_calc <- (1 / n_1)  + (d_calc ^ 2 / (2 * n_1))
-    es_method  <- "classic_cohen_d_within_two"
+    es_method  <- "d_within_two"
     
   } else if (participant_design == "within_one" & complete(x_1, x_2, n_1, SD_1)) {
     d_calc <- (x_1 - x_2) / SD_1
@@ -49,7 +49,7 @@ compute_es <- function(ma_df) {
   } else if (complete(d, d_var)) { 
     d_calc <- d
     d_var_calc <- d_var
-    es_method  <- "reported_d"
+    es_method  <- "reported_d_with_corrections"
   } 
   
   
